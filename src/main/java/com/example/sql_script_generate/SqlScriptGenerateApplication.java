07621862d3a60c461ad01f5a4ea1d691e5fce1fc
PM_User <danuka@paymedia.lk>
2026-04-10 22:47:46 +0530

    git init

---
+++ src/main/java/com/example/sql_script_generate/SqlScriptGenerateApplication.java
@@ -0,0 +1,13 @@
+package com.example.sql_script_generate;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SqlScriptGenerateApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SqlScriptGenerateApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/sql_script_generate/SqlScriptGenerateApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.sql_script_generate;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SqlScriptGenerateApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
